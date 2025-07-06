57b4b3438abd061d68743a42609b51ae44cf190c
Jeferson Silva <jeferson.i.silva@gmail.com>
2025-07-06 19:51:53 -0300

    feat: sanctum

---
+++ backend/app/app/Http/Controllers/AppUserController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AppUser;
+use Illuminate\Http\Request;
+
+class AppUserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AppUser $appUser)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, AppUser $appUser)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AppUser $appUser)
+    {
+        //
+    }
+}
--- backend/app/database/seeders/DatabaseSeeder.php
+++ backend/app/database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\AppUser;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,9 +15,15 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        $user = AppUser::firstOrNew(['id' => 1]);
+        $user->name = 'Admin';
+        $user->email = 'main@grr.la';
+        $user->password = 'main@grr.la';
+        $user->save();
     }
 }
